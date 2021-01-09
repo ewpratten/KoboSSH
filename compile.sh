d91432007e1a57651abdada14314a047af08d913
Evan Pratten <ewpratten@gmail.com>
2021-01-09 14:06:33 -0500

    Build CI

--- compile.sh
+++ compile.sh
@@ -5,15 +5,15 @@ set -e
 cd dropbear
 
 echo "Generating build tools"
-docker run -it -v $(pwd):/work ewpratten/kolib_toolchain:crosstools /bin/bash -c 'cd /work; autoconf; autoheader'
+docker run -v $(pwd):/work ewpratten/kolib_toolchain:crosstools /bin/bash -c 'cd /work; autoconf; autoheader'
 
 
 echo "Configuring dropbear to target arm-kobo-linux-gnueabihf"
-docker run -it -v $(pwd):/work ewpratten/kolib_toolchain:crosstools /bin/bash -c 'export PATH="/root/x-tools/arm-kobo-linux-gnueabihf/bin:$PATH"; cd /work; ./configure --host=arm-kobo-linux-gnueabihf --disable-zlib --enable-static'
+docker run -v $(pwd):/work ewpratten/kolib_toolchain:crosstools /bin/bash -c 'export PATH="/root/x-tools/arm-kobo-linux-gnueabihf/bin:$PATH"; cd /work; ./configure --host=arm-kobo-linux-gnueabihf --disable-zlib --enable-static'
 
 echo "Compiling dropbear"
-docker run -it -v $(pwd):/work ewpratten/kolib_toolchain:crosstools /bin/bash -c 'export PATH="/root/x-tools/arm-kobo-linux-gnueabihf/bin:$PATH"; cd /work; make clean'
-docker run -it -v $(pwd):/work ewpratten/kolib_toolchain:crosstools /bin/bash -c 'export PATH="/root/x-tools/arm-kobo-linux-gnueabihf/bin:$PATH"; cd /work; make PROGRAMS="dropbear dropbearkey" MULTI=1'
+docker run -v $(pwd):/work ewpratten/kolib_toolchain:crosstools /bin/bash -c 'export PATH="/root/x-tools/arm-kobo-linux-gnueabihf/bin:$PATH"; cd /work; make clean'
+docker run -v $(pwd):/work ewpratten/kolib_toolchain:crosstools /bin/bash -c 'export PATH="/root/x-tools/arm-kobo-linux-gnueabihf/bin:$PATH"; cd /work; make PROGRAMS="dropbear dropbearkey" MULTI=1'
 
 echo "Fetching binaries"
 cp dropbearmulti ..
